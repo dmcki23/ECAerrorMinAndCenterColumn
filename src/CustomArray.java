d4b63539263bbe7b90e7ce910c7454b165810345
dmcki23 <dmcki23@wgu.edu>
2025-02-25 18:43:24 -0500

    Have gathered the code from several local projects into this, initial commit

---
+++ src/CustomArray.java
@@ -0,0 +1,424 @@
+/**
+ * array display class
+ */
+public class CustomArray {
+    /**
+     * currently empty constructor
+     */
+    public CustomArray() {
+    }
+    public int[][] reflectRotateTranspose(int[][] in, int rotation) {
+        int size = in.length;
+        int[][] out = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
+                out[row][column] = in[row][column];
+            }
+        }
+        if (rotation % 2 == 1) {
+            int[][] nextTemp = new int[size][size];
+            for (int row = 0; row < size; row++) {
+                for (int column = 0; column < size; column++) {
+                    nextTemp[row][column] = out[size - 1 - row][column];
+                }
+            }
+            out = nextTemp;
+        }
+        if (((rotation / 2) % 2) == 1) {
+            int[][] nextTemp = new int[size][size];
+            for (int row = 0; row < size; row++) {
+                for (int column = 0; column < size; column++) {
+                    nextTemp[row][column] = out[row][size - 1 - column];
+                }
+            }
+            out = nextTemp;
+        }
+        if (((rotation / 4) % 2) == 1) {
+            int[][] nextTemp = new int[size][size];
+            for (int row = 0; row < size; row++) {
+                for (int column = 0; column < size; column++) {
+                    nextTemp[row][column] = out[column][row];
+                }
+            }
+            out = nextTemp;
+        }
+        return out;
+    }
+    /**
+     * Checks a 2D int array for Latin-ness, each row and column contains exactly one of each value. Used to help verify the integerity of the multiplication tables generated
+     *
+     * @param in 2D int array
+     * @return       boolean  true if in[][] is Latin and false if not
+     */
+    public static boolean isLatin(int[][] in) {
+        boolean isLatin = true;
+        for (int row = 0; row < in.length; row++) {
+            for (int column = 0; column < in.length; column++) {
+                for (int comp = 0; comp < in.length; comp++) {
+                    if (comp == column) continue;
+                    if (in[row][column] == in[row][comp]) isLatin = false;
+                }
+                for (int comp = 0; comp < in.length; comp++) {
+                    if (comp == row) continue;
+                    if (in[row][column] == in[comp][column]) isLatin = false;
+                }
+            }
+        }
+        return isLatin;
+    }
+    /**
+     * displays a 2D integer array
+     * @param in 2D integer array to display
+     * @param noZeros true if you want to replace zeros with spaces on output
+     * @param noSpaces true if you don't want any spaces between elements
+     */
+    public static void plusArrayDisplay(int[][] in, boolean noZeros, boolean noSpaces) {
+        int rows = in.length;
+        int columns = in[0].length;
+        String outstring = "";
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (in[row][column] == 0 && noZeros) {
+                    outstring += " ";
+                } else {
+                    outstring += in[row][column];
+                }
+                if (!noSpaces) {
+                    outstring += " ";
+                }
+            }
+            System.out.println(outstring);
+            outstring = "";
+        }
+        System.out.println();
+    }
+    /**
+     * displays a 2D integer array
+     * @param in 2D integer array to display
+     * @param noZeros true if you want to replace zeros with spaces on output
+     * @param noSpaces true if you don't want space padding on output
+     * @param label array title
+     */
+    public static void plusArrayDisplay(int[][] in, boolean noZeros, boolean noSpaces, String label) {
+        int rows = in.length;
+        int columns = in[0].length;
+        String outstring = "";
+        System.out.println(label);
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (in[row][column] == 0 && noZeros) {
+                    outstring += " ";
+                } else {
+                    outstring += in[row][column];
+                }
+                if (!noSpaces) {
+                    outstring += " ";
+                }
+            }
+            System.out.println(outstring);
+            outstring = "";
+        }
+        System.out.println();
+    }
+    /**
+     * displays a 2D integer array
+     * @param in 2D integer array to display
+     * @param noZeros true if you want to replace zeros with spaces on output
+     * @param noSpaces true if you don't want space padding on output
+     * @param label array title
+     */
+    public static void layersDisplay(int[][][] in, boolean noZeros, boolean noSpaces, String label) {
+        int rows = in.length;
+        int columns = in[0][0].length;
+        String outstring = "";
+        System.out.println(label);
+        for (int layer = 0; layer < in.length; layer++) {
+            System.out.println("Layer " + layer);
+            for (int row = 0; row < rows; row++) {
+                for (int column = 0; column < columns; column++) {
+                    if (in[layer][row][column] == 0 && noZeros) {
+                        outstring += " ";
+                    } else {
+                        outstring += in[layer][row][column];
+                    }
+                    if (!noSpaces) {
+                        outstring += " ";
+                    }
+                }
+                System.out.println(outstring);
+                outstring = "";
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    /**
+     * displays a 2D integer array on the console
+     * @param in 2D integer array to display
+     * @param noZeros true if you want to replace zeros with spaces on output
+     * @param noSpaces true if you don't want space padding on output
+     * @param rowDiv number of rows used on increment
+     * @param colDiv number of columns used on increment
+     */
+    public static void plusPlusArrayDisplay(int[][] in, boolean noZeros, boolean noSpaces, int rowDiv, int colDiv) {
+        int rows = in.length;
+        int columns = in[0].length;
+        String outstring = "";
+        for (int row = 0; row < rows; row += rowDiv) {
+            for (int column = 0; column < rows; column += colDiv) {
+                if (in[row][column] != 0) {
+                    outstring += in[row][column];
+                } else if (in[row][column] == 0 && noZeros) {
+                    outstring += " ";
+                } else {
+                    outstring += in[row][column];
+                }
+                if (!noSpaces) {
+                    outstring += " ";
+                }
+            }
+            System.out.println(outstring);
+            outstring = "";
+        }
+        System.out.println();
+    }
+    /**
+     * displays a 2D integer array
+     * @param in 2D integer array to display
+     */
+    public static void basicArrayDisplay(int[][] in) {
+        int rows = in.length;
+        int columns = in[0].length;
+        String outstring = "";
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                outstring += in[row][column];
+            }
+            System.out.println(outstring);
+            outstring = "";
+        }
+        System.out.println();
+    }
+    /**
+     * Applies a gray code to each axis of the input array
+     * @param in 2D input array
+     * @return input with Gray codes applied
+     */
+    public static int[][] grayIFY(int[][] in) {
+        int[][] out = new int[in.length][in.length];
+        int[] gray = PermutationsFactoradic.graySequence(8);
+        for (int row = 0; row < in.length; row++) {
+            for (int column = 0; column < in.length; column++) {
+                out[gray[row]][gray[column]] = in[row][column];
+            }
+        }
+        return out;
+    }
+    /**
+     * Same mediator layers from StirlingStacks
+     */
+    int[][][] intermediators;
+    /**
+     * Generates the intermediator array
+     * @param inDegree base-N, range of possibilities in each cell
+     * @param size array size
+     * @param layer not used atm
+     * @return 2D intermediator array
+     */
+    public int[][] conflictStackIntermediator(int inDegree, int size, int layer) {
+        int[][] out = new int[size][size];
+        intermediators = new int[5][size][size];
+        for (int row = 0; row < size; row++) {
+            out[row][row] = 1;
+            out[row][0] = 1;
+        }
+        for (int row = 1; row < size; row++) {
+            for (int column = 1; column < row; column++) {
+                out[row][column] = out[row - 1][column - 1] + out[row - 1][column] + out[row][column - 1];
+                out[row][column] = out[row][column] % inDegree;
+                intermediators[1][row][column] = out[row][column];
+                //intermediators[1][row][column] = ((intermediators[1][row][column] / (1 << layer)) % 2);
+            }
+        }
+        for (int row = 2; row < size; row++) {
+            for (int column = 1; column < row; column++) {
+                intermediators[1][row][column] /= 2;
+                intermediators[1][row][column] %= 2;
+                //intermediators[1][row][column] = ((intermediators[1][row][column] / (1 << layer)) % 2);
+            }
+        }
+        for (int row = 2; row < size; row += 2) {
+            //this constant valued intermediator is a conjugate vector
+            //because the 0-th position is 0 since it is greater than the diagonal
+            intermediators[2][row][1] = 1;
+        }
+//        System.out.println("150");
+//        System.out.println("Degree " + inDegree);
+//        for (int row = 0; row < out.length; row++) {
+//            for (int column = 0; column <= row; column++) {
+//                System.out.print(out[row][column]+" ");
+//            }
+//            System.out.print("\n");
+//        }
+//        System.out.print("\n");
+        return out;
+    }
+    /**
+     * A particular place of Pascal mod inDegree
+     * @param inDegree Pascal mod this
+     * @param size input array side length
+     * @param layer which place value layer to return
+     * @return Pascal mod inDegree, layer's place
+     */
+    public static int[][] stackPascalPlaceLayer(int inDegree, int size, int layer) {
+        int[][] out = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            out[row][row] = 1;
+            out[row][0] = 1;
+        }
+        for (int row = 2; row < size; row++) {
+            for (int column = 1; column < row; column++) {
+                out[row][column] = out[row - 1][column - 1] + out[row - 1][column];
+                out[row][column] = out[row][column] % inDegree;
+                //out[row][column] /= (int)Math.pow(2,layer-1);
+                //out[row][column] %= 2;
+            }
+        }
+        for (int row = 2; row < size; row++) {
+            for (int column = 1; column < row; column++) {
+//                out[row][column] = out[row - 1][column - 1] + (column % inDegree)*out[row - 1][column];
+//                out[row][column] = out[row][column] % inDegree;
+                out[row][column] /= (int) Math.pow(2, layer);
+                out[row][column] %= 2;
+            }
+        }
+//        System.out.println("Pascal");
+//        System.out.println("Degree " + inDegree);
+//        for (int row = 0; row < 100; row++) {
+//            for (int column = 0; column < 100; column++) {
+//                System.out.print(out[row][column]);
+//            }
+//            System.out.print("\n");
+//        }
+//        System.out.print("\n");
+//        for (int row = 2; row < size; row++) {
+//            for (int column = 1; column < row; column++) {
+//                for (int power = 0; power < inDegree / 2; power++) {
+//                    pascalStack[power][row][column] = (out[row][column] / (int) Math.pow(inDegree, power)) % 2;
+//                }
+//            }
+//        }
+        //pascal = out;
+        return out;
+    }
+    /**
+     * A particular place of Stirling mod inDegree
+     * @param inDegree Stirling mod this
+     * @param size input array side length
+     * @param layer which place value layer to return
+     * @return Stirling mod inDegree, layer's place
+     */
+    public static int[][] stackStirlingPlaceLayer(int inDegree, int size, int layer) {
+        int[][] out = new int[size][size];
+//        System.out.println("Calculating mod " + inDegree);
+//        for (int row = 0; row < size; row++) {
+//            out[row][0] = 1;
+//            out[row][row] = 1;
+//        }
+//        for (int row = 2; row < size; row++) {
+//            for (int column = 1; column < row; column++) {
+//                out[row][column] = (out[row - 1][column - 1] + (column % inDegree) * out[row - 1][column]) % inDegree;
+//                //out[row][column] = (out[row][column] / (int) Math.pow(2, layer)) % 2;
+//                //out[row][column] %= 2;
+////                    out[row][column] = out[row][column - 1] + degree * out[row - 1][column - 1] + degree * degree * out[row - 1][column];
+//                //                  out[row][column] = out[row][column] % degree;
+//            }
+//        }
+//        out = stackStirling(inDegree, size);
+//        for (int row = 0; row < size; row++) {
+//            for (int column = 0; column < size; column++) {
+////                out[row][column] = out[row - 1][column - 1] + (column % inDegree)*out[row - 1][column];
+////                out[row][column] = out[row][column] % inDegree;
+//                out[row][column] = out[row][column] / (int) Math.pow(2, layer);
+//                out[row][column] = out[row][column] % 2;
+//            }
+//        }
+//        //
+//        //basis shift from down, down-right to down-right
+////        for (int degree = 0; degree < 14; degree++) {
+////            for (int column = 1; column < size; column++) {
+////                for (int row = 0; row + column < size; row++) {
+////                    stirlingMod[degree][row][column] = stirlingMod[degree][row + column][column];
+////                }
+////            }
+////            for (int column = 1; column < size; column++) {
+////                for (int row = 0; row < column ; row++) {
+////                    stirlingMod[degree][4999-row][column] = 0;
+////                }
+////
+////            }
+////        }
+////        System.out.println("Stirling numbers");
+////        System.out.println("Degree " + inDegree);
+////        int[][] alt = new int[128][128];
+////        for (int row = 0; row < 128; row++) {
+////            for (int column = 0; column < 128; column++) {
+////                if (out[row+(column/2)][column] != 0) System.out.print(out[row+(column/2)][column]);
+////                else System.out.print(" ");
+////                alt[row][column] = out[row+(column/2)][column];
+////            }
+////            System.out.print("\n");
+////        }
+//        //stackConflict(2,alt,conflictStack,wolframStack);
+//        //findWolfram(alt,2,conflictStack,wolframStack,0);
+////        System.out.print("\n");
+////        for (int row = 2; row < size; row++) {
+////            for (int column = 1; column < row; column++) {
+////                for (int power = 0; power < inDegree / 2; power++) {
+////                    stirlingStack[power][row][column] = (out[row][column] / (int) Math.pow(inDegree, power)) % 2;
+////                }
+////            }
+////        }
+//        //stirling = out;
+        return out;
+    }
+    /**
+     * Reduces two input grids
+     * @param in input a
+     * @param stirlingIn input b
+     * @return a 'operation' b
+     */
+    public static int[][] reduce(int[][] in, int[][] stirlingIn) {
+        //pascal stirling at bottom
+        //AND pascal, AND stirling
+        //XOR pascal, XOR stirling
+        //etc
+        int[][] out = new int[in.length][in.length];
+        for (int row = 0; row < in.length; row++) {
+            for (int column = 0; column < in.length; column++) {
+                //out[row][column] = in[row][column] & ( (stirlingIn[row][column]+1)%2);
+                //out[row][column] = in[row][column] | stirlingIn[row][column];
+                out[row][column] = in[row][column] ^ stirlingIn[row][column];
+            }
+        }
+        return out;
+    }
+    /**
+     * A particular place value's layer of an input array
+     * @param in 2D grid
+     * @param layer which place value
+     * @param base which base
+     * @param modN which mod
+     * @return input with parameters applied
+     */
+    static int[][] layerOf(int[][] in, int layer,int base, int modN){
+        int[][] out = new int[in.length][in[0].length];
+        for (int row = 0; row < in.length; row++) {
+            for (int column = 0; column < in.length; column++) {
+                out[row][column] = ((in[row][column])/(int)Math.pow(base,layer))%modN;
+            }
+        }
+        return out;
+    }
+
+}
---
+++ src/ECAexponentReduction.java
@@ -0,0 +1,225 @@
+import java.util.Arrays;
+
+/**
+ * In an algorithm similar to long division, an ECA input neighborhood is plugged in, stretched, then fed back in to the previous row.
+ * It results in a repeat at some point because there are only so many possible input neighborhoods of length l,
+ * and from iteration 1 to some point the fedback neighborhoods produce an identical center column with a negative row offset of length iteration.
+ */
+public class ECAexponentReduction {
+    /**
+     * Utility functions for this class
+     */
+    RuleStretchTemplate ruleStretchTemplate = new RuleStretchTemplate();
+    /**
+     * At what iteration does the algorithm stop repeating?
+     */
+    int passRepeat;
+    /**
+     * Integer version of all solutions all rows for a specific iteration
+     */
+    int[][] decPasses;
+
+
+
+    /**
+     * Does the input, stretch, feedback algorithm to find offset equal center columns to the input neighborhood's center column
+     *
+     * @param n            ECA rule 0-255
+     * @param logIn        length of input neighborhood
+     * @param initialValue decimal value of input neighborhood
+     * @param feedbackRow  stretched to this row, then fed back into the feedbackRow-1 Wolfram code
+     * @return all solutions all rows all iterations
+     */
+    public int[][][] doLogReductionTwo(int n, int logIn, int initialValue, int feedbackRow) {
+        int[][][] passes = new int[32][24][64];
+        decPasses = new int[passes.length][passes[0].length];
+        int[] nw = new int[8];
+        for (int power = 0; power < 8; power++) {
+            nw[power] = ((n / (int) Math.pow(2, power)) % 2);
+        }
+
+        int maxRows = 22;
+        int value;
+        int initRow;
+        int[] values = new int[32];
+        values[0] = initialValue;
+        int lengthRepeat = -1;
+        for (int pass = 0; pass < 32 && lengthRepeat == -1; pass++) {
+            for (int row = 0; row < maxRows; row++) {
+                int powers = 1 + 2 * (row - 1);
+                for (int power = 0; power < powers; power++) {
+                    passes[pass][row][power] = -1;
+                }
+            }
+        }
+
+
+        value = initialValue;
+        initRow = logIn;
+        passLoop:
+        for (int pass = 0; pass < 32 && lengthRepeat == -1; pass++) {
+            int tot = 0;
+            for (int row = initRow; row < maxRows; row++) {
+                int powers = 1 + 2 * (row - 1);
+                for (int power = 0; power < powers; power++) {
+                    passes[pass][row][power] = nw[((value / (int) Math.pow(2, power)) % 8)];
+                }
+                value *= 2;
+            }
+
+            for (int row = 0; row < maxRows; row++) {
+                tot = 0;
+                int powers = 1 + 2 * (row - 1);
+                for (int power = 0; power < powers; power++) {
+                    tot += (int) Math.pow(2, power) * passes[pass][row][power];
+                }
+                decPasses[pass][row] = tot;
+            }
+            value = decPasses[pass][feedbackRow];
+            values[pass] = value;
+            for (int passs = 0; passs < pass; passs++) {
+                if (values[passs] == values[pass]) {
+                    lengthRepeat = pass;
+                    passRepeat = pass;
+                }
+            }
+            initRow = feedbackRow - 1;
+            System.out.print("\n");
+            System.out.println("passes[" + pass + "]");
+            for (int row = 0; row < 22; row++) {
+                for (int column = 0; column < 1 + 2 * (row - 1); column++) {
+                    System.out.print(String.format("%1d", passes[pass][row][column]));
+                }
+                System.out.print(" " + decPasses[pass][row] + "\n");
+            }
+            if (lengthRepeat != -1) break passLoop;
+            System.out.print("\n");
+        }
+
+        System.out.println("repeat " + passRepeat);
+        return passes;
+    }
+
+    /**
+     * ???
+     *
+     * @param n       ECA 0-255 rule
+     * @param numRows number of rows
+     * @return ???
+     */
+
+    public int[] backwardsColumn(int n, int numRows) {
+        int[] out = new int[numRows];
+        int[] multiplier = new int[]{1, 2, 4};
+        int[][] partial = new int[3][3];
+        int[] next = new int[3];
+        int[] in = new int[]{7, 3, 1};
+        int tot = 0;
+        for (int row = numRows - 1; row >= 0; row--) {
+            next = new int[3];
+            for (int value = 0; value < 3; value++) {
+                for (int mult = 0; mult < 3; mult++) {
+                    partial[value][mult] = in[value] / multiplier[mult];
+                    next[value] += (int) Math.pow(2, mult) * (((n / (int) Math.pow(2, partial[value][mult])) % 2));
+                }
+                //next[value] = ((n/(int)Math.pow(2,next[value]))%2);
+                in[value] = next[value];
+                next[value] = ((n / (int) Math.pow(2, next[value])) % 2);
+                out[row] += (int) Math.pow(2, value) * next[value];
+            }
+            out[row] = ((n / (int) Math.pow(2, out[row])) % 2);
+        }
+        return out;
+    }
+
+    /**
+     * ???
+     *
+     * @param n       ECA 0-255 rule
+     * @param numRows number of rows
+     */
+    public void checkColumns(int n, int numRows) {
+        int[][][] passes = doLogReductionTwo(n, 1, 2, 4);
+        int[] input = Arrays.copyOfRange(passes[passRepeat][5], 0, 9);
+        System.out.println(Arrays.toString(input));
+        int[] a = ruleStretchTemplate.traditionalCenterColumn(n, 512, input, 1024);
+        int[] b = ruleStretchTemplate.traditionalCenterColumn(n, 512, new int[]{1}, 1024);
+        for (int spot = 0; spot < 128; spot++) {
+            if (Arrays.equals(Arrays.copyOfRange(b, spot, spot + 30), Arrays.copyOfRange(a, 1, 31))) {
+                System.out.println("spot " + spot);
+            }
+        }
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+    }
+
+    /**
+     * ???
+     *
+     * @param n ECA 0-255 rule
+     */
+    public void checkAllColumns(int n) {
+        int[] a = ruleStretchTemplate.traditionalCenterColumn(n, 256, new int[]{1}, 512);
+        System.out.println(Arrays.toString(a));
+        int[] comp;
+        for (int l = 1; l < 2; l++) {
+            int numPowers = 2 * (l - 1) + 1;
+            for (int input = 2; input < 3; input++) {
+                for (int ll = 3; ll < 4; ll++) {
+                    int[][][] passes = doLogReductionTwo(n, 1, input, ll);
+                    System.out.println("passRepeat " + passRepeat);
+                    for (int pass = 0; pass <= passRepeat; pass++) {
+                        int[] neighborhood = Arrays.copyOfRange(passes[pass][ll + 1], 0, 2 * (ll) + 1);
+                        System.out.println(Arrays.toString(neighborhood) + " " + decPasses[pass][ll + 1]);
+                        comp = ruleStretchTemplate.traditionalCenterColumn(n, 256, neighborhood, 512);
+                        // comp = ruleStretchTemplate.traditionalCenterColumn(n,128,comp,256);
+                        //comp = Arrays.copyOfRange(comp,0,64);
+                        boolean found = false;
+                        for (int spot = 1; spot < 128; spot++) {
+                            int[] temp = Arrays.copyOfRange(comp, 1, 17);
+                            if (Arrays.equals(temp, Arrays.copyOfRange(a, spot, spot + 16))) {
+                                System.out.println("l " + l + " input " + input + " ll " + ll + " pass " + pass);
+                                System.out.println("spot " + spot);
+                                System.out.println();
+                                found = true;
+                            }
+                        }
+                        if (!found) {
+//                            System.out.println(Arrays.toString(Arrays.copyOfRange(comp, 1, 31)));
+//                            System.out.println(Arrays.toString(Arrays.copyOfRange(a, pass + 2, pass + 32)));
+//                            System.out.println();
+                        }
+                    }
+                    for (int in = 0; in < (int) Math.pow(2, 2 * (ll) + 1); in++) {
+                        int[] second = new int[2 * ll + 1];
+
+                        for (int index = 0; index < second.length; index++) {
+                            second[index] = ((in / (int) Math.pow(2, index)) % 2);
+                        }
+                        second = ruleStretchTemplate.traditionalCenterColumn(n, 256, second, 512);
+                        boolean found = false;
+                        for (int spot = 1; spot < 128; spot++) {
+                            int[] temp = Arrays.copyOfRange(second, 1, 17);
+                            if (Arrays.equals(temp, Arrays.copyOfRange(a, spot, spot + 16))) {
+                                System.out.println("spot " + spot + " in " + in + " middle three " + ((in / 8) % 8));
+//                                System.out.println("l " + l + " input " + input + " ll " + ll + " pass " + pass);
+//                                System.out.println("spot " + spot);
+                                System.out.println();
+                                found = true;
+
+                            }
+
+                        }
+                        if (!found) {
+
+//                            System.out.println(Arrays.toString(Arrays.copyOfRange(comp, 1, 31)));
+//                            System.out.println(Arrays.toString(Arrays.copyOfRange(a, pass+2, pass + 32)));
+                            System.out.println();
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
